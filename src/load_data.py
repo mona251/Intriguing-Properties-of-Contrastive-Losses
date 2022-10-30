e4a4581a7106b281c7e8f4f06c5103aaa49c7691
Luca Marini <lucamarini1998@gmail.com>
2022-10-30 16:26:02 +0100

    add function to show img from a pytorch dataset

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,14 @@
 from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def load_mnist():
     mnist_dataset_train = MNIST('../data/mnist/', download=True)
     mnist_dataset_test = MNIST('../data/mnist/', download=True, train=False)
     return mnist_dataset_train, mnist_dataset_test
+
+
+def show_ith_image(dataset, i):
+    x, _ = dataset[i]
+    plt.imshow(np.asarray(x), cmap='gray_r')
