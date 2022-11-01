e8b067ca2f81bebffa7996586166826dcb949953
Luca Marini <lucamarini1998@gmail.com>
2022-11-01 09:24:59 +0100

    first version of multi digits with different shape works

--- src/create_data.py
+++ src/create_data.py
@@ -6,6 +6,38 @@ import cv2 as cv
 import random
 
 
+def overlay_img(small_img, large_img, is_large_img_grayscale,
+                bb_start_h, bb_end_h, bb_start_w, bb_end_w):
+    """
+    Overlays a small_img on a large_img at a specified bounding box.
+    Args:
+        small_img: small image
+        large_img: large image
+        is_large_img_grayscale: True if large_img is grayscale
+        bb_start_h: y coordinate of the top corners of the bounding box
+         inside large_img that will contain small_img
+        bb_end_h: y coordinate of the bottom corners of the bounding box
+         inside large_img that will contain small_img
+        bb_start_w: x coordinate of the left corners of the bounding box
+         inside large_img that will contain small_img
+        bb_end_w: x coordinate of the right corners of the bounding box
+         inside large_img that will contain small_img
+    """
+    if is_large_img_grayscale:
+        large_img[bb_start_h:bb_end_h, bb_start_w:bb_end_w] = \
+            (small_img + large_img[bb_start_h:bb_end_h,
+                         bb_start_w:bb_end_w])
+    else:
+        small_img_rgb = cv.cvtColor(small_img, cv.COLOR_GRAY2RGB)
+        alpha_s = small_img_rgb[:, :, 2] / 255.0
+        alpha_l = 1.0 - alpha_s
+        for c in range(large_img.shape[-1]):
+            large_img[bb_start_h:bb_end_h, bb_start_w:bb_end_w, c] = \
+                (alpha_s * small_img_rgb[:, :, c] +
+                 alpha_l * large_img[bb_start_h:bb_end_h,
+                           bb_start_w:bb_end_w, c])
+
+
 def overlay_small_img_on_large_img_grid(small_img, large_img,
                                         num_cells_grid,
                                         n_repetitions_small_img,
@@ -38,8 +70,6 @@ def overlay_small_img_on_large_img_grid(small_img, large_img,
 
     small_img = downsample_img(small_img, height_size_cell, width_size_cell,
                                grayscale=True)
-    if not is_large_img_grayscale:
-        backtorgb = cv.cvtColor(small_img, cv.COLOR_GRAY2RGB)
 
     col_idx = 0
     row_idx = 0
@@ -47,7 +77,7 @@ def overlay_small_img_on_large_img_grid(small_img, large_img,
 
     final_img = np.copy(large_img)
 
-    #random.seed(seed)
+    # random.seed(seed)
     sampled_idxs_of_cells = random.sample(range(num_cells_in_grid),
                                           n_repetitions_small_img)
 
@@ -59,18 +89,8 @@ def overlay_small_img_on_large_img_grid(small_img, large_img,
             cell_end_h = (row_idx + 1) * int(height_size_cell)
             cell_end_w = (col_idx + 1) * int(width_size_cell)
 
-            if is_large_img_grayscale:
-                final_img[cell_start_h:cell_end_h, cell_start_w:cell_end_w] = \
-                    (small_img + final_img[cell_start_h:cell_end_h,
-                                           cell_start_w:cell_end_w])
-            else:
-                alpha_s = backtorgb[:, :, 2] / 255.0
-                alpha_l = 1.0 - alpha_s
-                for c in range(final_img.shape[-1]):
-                    final_img[cell_start_h:cell_end_h, cell_start_w:cell_end_w, c] = \
-                        (alpha_s * backtorgb[:, :, c] +
-                         alpha_l * final_img[cell_start_h:cell_end_h,
-                                             cell_start_w:cell_end_w, c])
+            overlay_img(small_img, final_img, is_large_img_grayscale,
+                        cell_start_h, cell_end_h, cell_start_w, cell_end_w)
         counter += 1
         col_idx += 1
         if counter % num_cells_one_row == 0:
@@ -85,3 +105,74 @@ def overlay_small_img_on_large_img_grid(small_img, large_img,
         plt.show()
 
     return final_img
+
+
+def overlay_small_img_on_large_img_at_random_position(big_img, small_img):
+    """
+
+    Args:
+        big_img: big image that will contain small_img
+        small_img: small image that will be added to big
+
+    Returns:
+
+    """
+    half_height_small_img = small_img.shape[0] // 2
+    half_width_small_img = small_img.shape[1] // 2
+
+    big_img_h, big_img_w = big_img.shape[:2]
+    # range of coordinates of the portion of the large_img that can
+    # contain the small_img
+    top_left_coords_range = (half_height_small_img + 1, half_width_small_img + 1)
+    bottom_right_coords_range = (big_img_h - half_height_small_img - 1,
+                                 big_img_w - half_width_small_img - 1)
+    # coordinates of where the center of the small_img will be
+    # wrt the large_img
+    center_y = np.random.randint(top_left_coords_range[0],
+                                 bottom_right_coords_range[0])
+    center_x = np.random.randint(top_left_coords_range[1],
+                                 bottom_right_coords_range[1])
+    # Coordinates of the portion of the large_img that will contain the
+    # small_img (h, w)
+    top_left_coord_small_img = (center_y - half_height_small_img,
+                                center_x - half_width_small_img)
+    bottom_right_coord_small_img = (center_y + half_height_small_img,
+                                    center_x + half_width_small_img)
+    # height of the bounding box that will contain the small image
+    height_bb_small_img = \
+        bottom_right_coord_small_img[0] - top_left_coord_small_img[0]
+    width_bb_small_img = \
+        bottom_right_coord_small_img[1] - top_left_coord_small_img[1]
+    if height_bb_small_img != small_img.shape[0]:
+        i = 0
+        while height_bb_small_img < small_img.shape[0]:
+            i += 1
+            bottom_right_coord_small_img = (bottom_right_coord_small_img[0] + i,
+                                            bottom_right_coord_small_img[1])
+            height_bb_small_img = \
+                bottom_right_coord_small_img[0] - top_left_coord_small_img[0]
+
+        while height_bb_small_img > small_img.shape[0]:
+            i += 1
+            bottom_right_coord_small_img = (bottom_right_coord_small_img[0] - i,
+                                            bottom_right_coord_small_img[1])
+            height_bb_small_img = \
+                bottom_right_coord_small_img[0] - top_left_coord_small_img[0]
+
+    if width_bb_small_img != small_img.shape[1]:
+        i = 0
+        while width_bb_small_img < small_img.shape[1]:
+            i += 1
+            bottom_right_coord_small_img = (bottom_right_coord_small_img[0],
+                                            bottom_right_coord_small_img[1] + i)
+            width_bb_small_img = \
+                bottom_right_coord_small_img[1] - top_left_coord_small_img[1]
+        while width_bb_small_img > small_img.shape[1]:
+            i += 1
+            bottom_right_coord_small_img = (bottom_right_coord_small_img[0],
+                                            bottom_right_coord_small_img[1] - i)
+            width_bb_small_img = \
+                bottom_right_coord_small_img[1] - top_left_coord_small_img[1]
+
+    big_img[top_left_coord_small_img[0]:bottom_right_coord_small_img[0],
+            top_left_coord_small_img[1]:bottom_right_coord_small_img[1]] = small_img
