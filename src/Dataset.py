b9bd1aded0d10717a57c683768e074cd8a33c92b
Luca Marini <lucamarini1998@gmail.com>
2022-11-02 19:49:19 +0100

    create images with 2 digits randomly placed with varying sizes

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,6 +1,6 @@
 import numpy as np
 from src.load_data import load_mnist, sample_uniformly_imgs
-from src.utils import crop_digit
+from src.utils import crop_digit, downsample_img
 from src.create_data import overlay_small_img_on_large_img_at_random_position
 from src.create_data import overlay_small_img_on_large_img_grid
 
@@ -13,7 +13,7 @@ class Dataset(object):
                                               img_shape=(112, 112)):
         """
         Generates a dataset with images with multi digits randomly placed on
-        images.
+        black images.
         Args:
             n_images: number of images to generate
             n_digits_in_img: number of digits for each image
@@ -86,6 +86,58 @@ class Dataset(object):
                     n_repetition_digit_img, is_large_img_grayscale=True,
                     repeat_small_img=False, dataset=self.mnist_dataset_train,
                     plot=False)
+            dataset.append(canvas)
+
+        return dataset
+
+    def generate_two_digits_varying_size_randomly_placed(
+            self, n_images, img_shape=(112, 112), min_size=20, max_size=80):
+        """
+        Generates a dataset with images with two digits randomly placed on
+        black images.
+        Args:
+            n_images: number of images to generate
+            img_shape: shape of each image
+            min_size: minimum size of the digit
+            max_size: maximum size of the digit
+
+        Returns:
+            List containing the generated images.
+        """
+        n_digits = 2
+        canvas_height = img_shape[0]
+        canvas_width = img_shape[1]
+
+        dataset = []
+
+        for img_idx in range(n_images):
+            canvas = np.zeros((canvas_height, canvas_width))
+
+            sampled_digit_images = sample_uniformly_imgs(
+                self.mnist_dataset_train, n_digits)
+
+            first_digit = sampled_digit_images[0]
+            second_digit = sampled_digit_images[1]
+
+            min_size = min_size
+            max_size = max_size
+            scale = 10
+            size_first_digit = min_size
+            size_second_digit = np.random.randint(
+                min_size / scale, (max_size / scale) + 1) * scale
+
+            first_digit = downsample_img(first_digit, size_first_digit,
+                                         size_first_digit, grayscale=True)
+            second_digit = downsample_img(second_digit, size_second_digit,
+                                          size_second_digit, grayscale=True)
+            first_digit_cropped = crop_digit(first_digit, plot=False)
+            second_digit_cropped = crop_digit(second_digit, plot=False)
+
+            overlay_small_img_on_large_img_at_random_position(
+                canvas, second_digit_cropped, is_large_img_grayscale=True)
+            overlay_small_img_on_large_img_at_random_position(
+                canvas, first_digit_cropped, is_large_img_grayscale=True)
+
             dataset.append(canvas)
 
         return dataset
